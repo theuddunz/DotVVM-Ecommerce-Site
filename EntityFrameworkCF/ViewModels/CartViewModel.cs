619c5ebf491a89e2e2df75f0146bd65e4dd7e039
iizMerk <merkmoi2@gmail.com>
2016-11-22 11:51:23 +0100

    Added the Cart Page

---
+++ EntityFrameworkCF/ViewModels/CartViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+
+namespace EntityFrameworkCF.ViewModels
+{
+	public class CartViewModel : DotvvmViewModelBase
+	{
+	}
+}
+
