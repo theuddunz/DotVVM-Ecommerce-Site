c2146670b53f9b7e64be0d2538ac24f55acb05bf
iizMerk <merkmoi2@gmail.com>
2016-11-24 17:18:20 +0100

    Added in the configuration of the Database the method for automactly create one GuestUser and 1AdminUser when the database is empty.

--- EntityFrameworkCF/Migrations/Configuration.cs
+++ EntityFrameworkCF/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
+using EntityFrameworkCF.ViewModels;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 namespace EntityFrameworkCF.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+    
 
     internal sealed class Configuration : DbMigrationsConfiguration<EntityFrameworkCF.ViewModels.Database>
     {
@@ -15,18 +17,27 @@ namespace EntityFrameworkCF.Migrations
 
         protected override void Seed(EntityFrameworkCF.ViewModels.Database context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            
+            if (!context.Users.Any())
+            {
+                var GuestUser = new User
+                {
+                    Username = "GuestUser",
+                    Email = "guestuser@guest.com",
+                    Password = "guestguest",                   
+                    UserRole = UserRole.User                    
+                };
+                var AdminUser = new User
+                {
+                    Username = "AdminUser",
+                    Email = "adminuser@admin.com",
+                    Password = "adminadmin",
+                    UserRole = UserRole.Admin
+                };
+                context.Users.Add(GuestUser);
+                context.Users.Add(AdminUser);
+                context.SaveChanges();
+            }
         }
     }
 }
