aa0d695bcce5beb173036e3d4c193a08e42a0c45
iizMerk <merkmoi2@gmail.com>
2016-12-02 15:08:41 +0100

    Added a "TestProduct" when the Products Table is empty.

--- EntityFrameworkCF/Migrations/Configuration.cs
+++ EntityFrameworkCF/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ using System.Data.Entity.Migrations;
 using System.Linq;
 namespace EntityFrameworkCF.Migrations
 {
-    
+
 
     internal sealed class Configuration : DbMigrationsConfiguration<EntityFrameworkCF.ViewModels.Database>
     {
@@ -17,15 +17,15 @@ namespace EntityFrameworkCF.Migrations
 
         protected override void Seed(EntityFrameworkCF.ViewModels.Database context)
         {
-            
+
             if (!context.Users.Any())
             {
                 var GuestUser = new User
                 {
                     Username = "GuestUser",
                     Email = "guestuser@guest.com",
-                    Password = "guestguest",                   
-                    UserRole = UserRole.User                    
+                    Password = "guestguest",
+                    UserRole = UserRole.User
                 };
                 var AdminUser = new User
                 {
@@ -34,10 +34,25 @@ namespace EntityFrameworkCF.Migrations
                     Password = "adminadmin",
                     UserRole = UserRole.Admin
                 };
+
+
                 context.Users.Add(GuestUser);
                 context.Users.Add(AdminUser);
                 context.SaveChanges();
             }
+            if (!context.Products.Any())
+            {
+                var product = new Product
+                {
+                    Name = "TestProduct",
+                    Description = "TestDescription",
+                    Price = 9999,
+                    Image = "https://static1.squarespace.com/static/5346b528e4b0e4b25f532794/t/54ecb349e4b0a3ebc2cfdcc6/1424798562058/ifttt-animated-gif.gif",
+                };
+                context.Products.Add(product);
+                context.SaveChanges();
+                }
+
         }
     }
 }
